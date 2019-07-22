2109318890ee96b1d8ffa3e677f4fce3fbbe8fa7
Kenney Knisely <kknisely@mac.com>
2019-07-21 20:09:04 -0400

    Add class of IceCream

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 class Desserts {
-  constructor(type, calories){
+  constructor(type,calories){
     this.type = type;
     this.calories = calories;
+  }
+}
+
+class IceCream extends Desserts {
+  constructor(type,calories,flavor,scoops){
+    super(type,calories);
+    this.flavor = falvor;
+    this.scoops = scoops;
+  }
+  includeSpoon(){
+    console.log("Here is your spoon!");
   }
 }
